b188a1976e3c8c9aedd2cf6cf6d53a75026db668
joseroshan <jose.roshanjose@gmail.com>
2014-12-21 18:50:25 -0500

    First Commit

---
+++ plot1.R
@@ -0,0 +1,10 @@
+# Reading data
+NEI <- readRDS("./data/exdata_data_NEI_data/summarySCC_PM25.rds")
+# Create Plot-Data
+pd <- aggregate(NEI[c("Emissions")], list(year = NEI$year), sum)
+# Create Plot 1
+png('plot1.png', width=480, height=480)
+plot(pd$year, pd$Emissions, type = "l",
+     main = "Total Emissions from PM2.5 in the US",
+     xlab = "Year", ylab = "Emissions")
+dev.off()
